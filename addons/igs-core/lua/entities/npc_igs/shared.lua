ddc8bc9630f02fed3fb7e1275b5136bb51629a0a
gefake <gefakeo@gmail.com>
2021-08-20 14:33:20 +0300

    Запрещаем спавнить нпс

--- addons/igs-core/lua/entities/npc_igs/shared.lua
+++ addons/igs-core/lua/entities/npc_igs/shared.lua
@@ -3,6 +3,6 @@ ENT.Type      = "ai"
 ENT.PrintName = "Донат NPC"
 ENT.Author    = "gm-donate.ru"
 ENT.Category  = "IGS"
-ENT.Spawnable = true
+ENT.Spawnable = false
 
 ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
